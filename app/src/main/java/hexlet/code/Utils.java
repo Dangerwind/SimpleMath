f6ec7f56121ea47bb693a6dc2305a2b0f067e306
Dangerwind <dangerwind@gmail.com>
2024-10-20 19:26:17 +0300

    some fix

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -9,7 +9,9 @@ public class Utils {
     }
 
     public static int euclidCalculation(int aNumber, int bNumber) {
-        if (bNumber == 0) throw new RuntimeException();
+        if (bNumber == 0) {
+            throw new RuntimeException();
+        }
         int remainder = aNumber % bNumber;
         while (remainder > 0) {
             aNumber = bNumber;
