b5db3f5330ba4e6e56c35d361fcaa6bd137a8ebe
Dangerwind <dangerwind@gmail.com>
2024-10-23 12:36:28 +0300

    fix: some methods removed

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,33 +1,9 @@
 package hexlet.code;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Utils {
     public static int getRandomInt(int minValue, int maxValue) {
         return new Random().nextInt(minValue, maxValue);
-    }
-
-    public static int euclidCalculation(int aNumber, int bNumber) {
-        if (bNumber == 0) {
-            throw new RuntimeException();
-        }
-        int remainder = aNumber % bNumber;
-        while (remainder > 0) {
-            aNumber = bNumber;
-            bNumber = remainder;
-            remainder = aNumber % bNumber;
-        }
-        return bNumber;
-    }
-
-    public static String getWelcomeName() {
-        System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
-
-        Scanner scanner = new Scanner(System.in);
-        String userName = scanner.next();
-        System.out.println("Hello, " + userName + "!");
-        return userName;
     }
 }
