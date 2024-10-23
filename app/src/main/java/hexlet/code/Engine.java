97ee72fd55fc1ac0df3c346194c784eb06d6e8ca
Dangerwind <dangerwind@gmail.com>
2024-10-23 12:35:02 +0300

    fix: no classes, only arrays

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -8,23 +8,30 @@ public class Engine {
     // количество раундов любой игры, по умолчанию 3
     public static final int ROUNDS = 3;
 
-    public static void run(GameData base) {
-        base.setUserName(Utils.getWelcomeName());
-        System.out.println(base.getRules());
+    public static void run(String[][] gameData, String rules) {
+      //  base.setUserName(Utils.getWelcomeName());
+
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+
+        System.out.println(rules);
         for (int i = 0; i < ROUNDS; i++) {
-            System.out.println("Question: " + base.getQuestion(i));
+            System.out.println("Question: " + gameData[i][0]);
             System.out.print("Your answer: ");
-            Scanner scanner = new Scanner(System.in);
             String answer = scanner.next();
-            if (answer.equals(base.getAnswer(i))) {
+            if (answer.equals(gameData[i][1])) {
                 System.out.println("Correct!");
             } else {
                 System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '"
-                        + base.getAnswer(i) + "'");
-                System.out.println("Let's try again, " + base.getUserName() + "!");
+                        + gameData[i][1] + "'");
+                System.out.println("Let's try again, " + userName + "!");
                 return;
             }
         }
-        System.out.println("Congratulations, " + base.getUserName() + "!");
+        System.out.println("Congratulations, " + userName + "!");
     }
 }
